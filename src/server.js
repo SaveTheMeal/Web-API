4275e7e9f3f5b6dc716d35ae94278b80aac78c3e
Alessio Novel <alessio.novel@studenti.unitn.i>
2022-12-22 15:38:56 +0100

    Modifiche

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const swaggerDocument = require("./swagger.json");
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //establish connection to database
+mongoose.set('strictQuery', false);
 mongoose.connect(
   process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
