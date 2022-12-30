c7f2771412f560ce320149728486145fb18ebeca
Alessia Laghezza <4le141@gmail.com>
2022-12-30 10:32:58 +0100

    aggiornamenti vari

--- backend/server.test.js
+++ backend/server.test.js
@@ -4,6 +4,7 @@
 const request = require('supertest');
 const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const app     = require('./server');
+const mongoose = require('mongoose');
 
 test('app module should be defined', async () => {
   expect(app).toBeDefined();
