cd6a3db0114cbc4cb97540896aee3c4c625c9c6d
Alessia Laghezza <4le141@gmail.com>
2022-12-31 18:50:07 +0100

    adjusted linstening port

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ mongoose.connect(
   }
 );
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
